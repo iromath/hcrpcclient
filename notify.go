9d5db1514fa703d42fd348cd4cdd24dd6ba17e1b
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 22:07:01 +0700

    Update notify.go

--- notify.go
+++ notify.go
@@ -14,10 +14,10 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/james-ray/hcd/chaincfg/chainhash"
-	"github.com/james-ray/hcd/hcjson"
-	"github.com/james-ray/hcd/hcutil"
-	"github.com/james-ray/hcd/wire"
+	"github.com/iromath/hcd/chaincfg/chainhash"
+	"github.com/iromath/hcd/hcjson"
+	"github.com/iromath/hcd/hcutil"
+	"github.com/iromath/hcd/wire"
 )
 
 var (
